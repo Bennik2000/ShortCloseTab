8420cbe02a6c7c2c4b482645a96fad7f09b02238
Bennik2000 <bennik.ko@gmail.com>
2016-04-01 21:33:04 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Roadmap
+
+- [x] Close the active document by pressing Ctrl+W twice
+- [ ] Close the active document by pressing Ctrl+W once
+
+Features that have a checkmark are complete and available for
+download in the
+[CI build](http://vsixgallery.com/extension/c9675081-d0b9-4176-ad00-5aebfc7fdec0/).
+
+# Changelog
+
+These are the changes to each version that has been released
+on the official Visual Studio extension gallery.
+
+## 1.0
+
+**2016-04-01**
+
+- [x] Initial release
+- [x] Close the active document by pressing Ctrl+W twice
---
+++ ShortCloseTab/CloseTabCommand.cs
@@ -0,0 +1,54 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+
+namespace ShortCloseTab
+{
+    internal sealed class CloseTabCommand
+    {
+        private const int CommandId = 0x0100;
+
+        private static readonly Guid CommandSet = new Guid("286f10fc-2b99-49a7-b8f9-04ee335b6bba");
+        
+        private readonly Package _package;
+        
+        private CloseTabCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            _package = package;
+
+            OleMenuCommandService commandService = ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandId = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandId);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        private IServiceProvider ServiceProvider => _package;
+
+        public static void Initialize(Package package)
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            new CloseTabCommand(package);
+        }
+        
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var dte = (DTE2) ServiceProvider.GetService(typeof (DTE));
+            try
+            {
+                dte.ExecuteCommand("close");
+            }
+            // ReSharper disable once EmptyGeneralCatchClause
+            catch { }
+        }
+    }
+}
